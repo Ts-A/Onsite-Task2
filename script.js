ae556c02c17d4564c944a09eeb1df747881cc722
TSajeet <codewithstatus200@gmail.com>
2020-08-21 10:54:40 +0530

    No style commit

--- script.js
+++ script.js
@@ -1,29 +1,48 @@
-let pattern =  /^(o|re)*(o|re|&)*(o|re)$/i
-
-const submit = document.querySelector('#submit'),
+const pattern =  /^(o|re)*(o|re|&)*(o|re)$/i,
+      submit = document.querySelector('#submit'),
       oreo   = document.querySelector('#oreo'),
-      cookie = document.querySelector('.cookie')
+      cookie = document.querySelector('.cookie'),
+      stack = document.querySelector('.stack'),
+      select = document.querySelector('#select')
+
+var cookies      = document.querySelectorAll('.cookie'),
+    selectCookie = 0
+
+select.addEventListener("change",() => {
+  let sv = Number(select.value)
+  selectCookie = sv - 1
+  if( sv > cookies.length ){
+    let element = document.createElement("div")
+    
+    element.classList.add('cookie')
+    stack.appendChild(element)
+    cookies = document.querySelectorAll('.cookie')
+    
+    let option = document.createElement("option")
+    
+    option.appendChild( document.createTextNode(`${sv+1}`) )
+    option.value = `${sv+1}`
+    select.appendChild(option)
+  }
+  else{
+  }
+})
 
 submit.addEventListener('click', () => {
-  cookie.innerHTML = ''
-  if( validate( oreo.value , pattern  ) )
-    console.log("Proceed")
-  else{
+  cookies[selectCookie].innerHTML = ''
+  if( !validate( oreo.value , pattern  ) ){
     oreo.value = ''
-    oreo.placeholder = 'Enter only from O,RE,& and do not start with or end with &'
-    oreo.focus()
-    return console.log("Try AGAIN")  
-}
-
-let encoded  = encoder( oreo.value.toUpperCase() )
-  let children = decoder( encoded )
-  
-  console.log(cookie)
-  for( i of children ){
-    cookie.appendChild(i)
+    oreo.placeholder = 'Enter only from O,RE,& and do not start or end with &'
+    return oreo.focus()
   }
-  cookie.style.height = `${ ( children.length+2 )*10 + 100}px`
-  html.style.height = "100px"
+
+  let encoded  = encoder( oreo.value.toUpperCase() ),
+      children = decoder( encoded )
+  for( i of children ){
+    cookies[selectCookie].appendChild( i )
+  }
+  cookies[selectCookie].style.height = `${ ( children.length + 2 )*10 + 100}px`
+  dragElement( cookies[selectCookie] );
 })
 
 const validate = ( value  , regex ) => {
@@ -41,9 +60,9 @@ const encoder = ( elem ) => {
 }
 
 const decoder = ( elem ) => {
-  let array = []
-  let length = 100
-  let z = elem.length
+  let array = [],
+      length = 100,
+      z = elem.length
   for( i of elem ){
     let x = ( document.createElement("img") )
     if( i == 'O' ){
@@ -53,12 +72,14 @@ const decoder = ( elem ) => {
         length -= 90
         x.style.top = `${length}px`
         x.style.zIndex = z-- 
+        x.draggable = true
       } else{
         x.classList.add("new")
         x.src = 'top.png'
         length -= 90
         x.style.top = `${length}px`
-        x.style.zIndex = z-- 
+        x.style.zIndex = z--
+        x.draggable = true
       }
     }
     else if( i == 'R' ){
@@ -66,7 +87,8 @@ const decoder = ( elem ) => {
       x.src = 'middle.png'
       length -= 90
       x.style.top = `${length}px`
-      x.style.zIndex = z-- 
+      x.style.zIndex = z--
+      x.draggable = true
     }
     else{
       x.classList.add("new")
@@ -76,10 +98,42 @@ const decoder = ( elem ) => {
       x.style.top = `${length}px`
       x.style.zIndex = z-- 
       x.style.opacity = 0
+      x.draggable = true
     }
-    console.log(length)
     array.push(x)
   } 
-  console.log(array)
   return array
+}
+
+const dragElement = ( oreo ) => {
+  var fx = 0, 
+      fy = 0, 
+      ix = 0, 
+      iy = 0
+
+  // oreo.addEventListener('mousedown',dragMouseDown)    
+  oreo.onmousedown = dragMouseDown;     //ClickDown
+
+  function dragMouseDown( e ) {
+    e.preventDefault()
+    ix = e.clientX
+    iy = e.clientY
+    document.onmouseup = closeDragElement
+    document.onmousemove = elementDrag
+  }
+
+  function elementDrag( e ) {
+    e.preventDefault() 
+    fx = ix - e.clientX
+    fy = iy - e.clientY
+    ix = e.clientX
+    iy = e.clientY
+    oreo.style.top = `${oreo.offsetTop - fy}px`
+    oreo.style.left = `${oreo.offsetLeft - fx}px`
+  }
+
+  function closeDragElement() {
+    document.onmouseup = null
+    document.onmousemove = null
+  }
 }
